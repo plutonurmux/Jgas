63ed660f4e566102fb93e106bb45fc9e05f00856
hinojos1194 <kevinhinojos1111@gmail.com>
2018-06-16 17:01:25 -0300

    Cochise Footer

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.css']
 })
-export class FooterComponent implements OnInit {
+export class FooterComponent implements OnInit{
+
+  public isReady: boolean = false;
 
   constructor() { }
 
   ngOnInit() {
+    setTimeout( () => this.isReady = true, 2500);
   }
 
 }
