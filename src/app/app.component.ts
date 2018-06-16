b0003d26cd859c4698cf1974cdec15b6c4d3be16
Raphael Luiz Martins Koszalka <rahael@getbyond.com>
2018-06-16 13:56:47 -0300

    Translation working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,10 +13,8 @@ import {TranslateService} from '@ngx-translate/core';
 export class AppComponent implements OnInit {
 
   constructor(private translate: TranslateService, private broadcaster: BroadcasterService) {
-    // this language will be used as a fallback when a translation isn't found in the current language
+    // @todo: save selected lang at localStorage and then load it
     translate.setDefaultLang('pt_BR');
-
-    // the lang to use, if the lang isn't available, it will use the current loader to get them
     translate.use('pt_BR');
   }
 
