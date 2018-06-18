faed8bd35c91612d3d49858ae0472edd6ad00db6
Raphael Luiz Martins Koszalka <rahael@getbyond.com>
2018-06-18 00:05:36 -0300

    Fix properties scroll

--- src/app/components/property/property.component.ts
+++ src/app/components/property/property.component.ts
@@ -23,12 +23,46 @@ export class PropertyComponent implements AfterViewInit {
     itemsContainer.style.width = itemsContainerWidth + 'px';
   }
 
+  private scrollItems(onRight: boolean): void {
+    const itemsContainer: HTMLElement = document.getElementById('itemsList');
+
+    if (onRight) {
+      let marginLeft = 300;
+      setInterval( () => {
+        if (itemsContainer.style.width < itemsContainer.style.marginLeft) {
+          itemsContainer.style.marginLeft = marginLeft + 'px';
+          marginLeft += 300;
+        }
+      }, 200);
+    }
+
+  }
+
   public sumBedrooms(estate): number {
     return parseInt(estate['imovel_dormitorio'], 10) + parseInt(estate['imovel_suite'], 10);
   }
 
-  public propertiesHorizontalScroll(direction: string): void {
+  public propertiesHorizontalScroll(isLeaving: boolean): void {
+    const itemsContainer: HTMLElement = document.getElementById('itemsList');
+    itemsContainer.addEventListener('mousemove', (e) => {
 
+      if (!isLeaving) {
+
+        if (window.innerWidth - e.screenX < 40) {
+          this.scrollItems(true);
+          return;
+        }
+
+        if ((window.innerWidth - e.screenX) < (window.innerWidth - 40)) {
+          this.scrollItems(false);
+          return;
+        }
+      }
+    });
+
+    if (isLeaving) {
+      itemsContainer.removeEventListener('mousemove', (e) => { console.log('Event canceled.'); });
+    }
   }
 
 
